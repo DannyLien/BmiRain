ac4dc4e008af05f77e2e25e8b3bdf028c3034dd9
DannyLien <danny.lien1109@gmail.com>
2024-04-10 14:35:37 +0800

    initial commit

---
+++ app/src/main/java/com/hank/rainbmi/Hello.kt
@@ -0,0 +1,17 @@
+package com.hank.rainbmi
+
+class Hello {
+
+
+}
+
+fun main() {
+    println("Hello")    //Hello
+    val age = 20
+    println(age.inc())  //21
+    println(age)        //20
+    var name = "Hank"
+    println(name.get(1))    //a
+    name = "Jack"
+    println(name)       //Jack
+}
