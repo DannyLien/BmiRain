a1176b2ed1dd537f492eb901d6040a2eb4f5211f
DannyLien <danny.lien1109@gmail.com>
2024-04-20 22:18:05 +0800

    3-6, average(), grading(), listOf()集合, for迴圈;

--- app/src/main/java/com/hank/rainbmi/student/Student.kt
+++ app/src/main/java/com/hank/rainbmi/student/Student.kt
@@ -8,20 +8,46 @@ package com.hank.rainbmi.student
 class Student(
     val id: String,
     val name: String,
-    var english: Int,
-    var math: Int
+    var english: Int = 0,
+    var math: Int = 0
 ) {
-    constructor(id: String, name: String) : this(id, name, 0, 0)
+//    constructor(id: String, name: String) : this(id, name, 0, 0)
 
     fun print() {
-        println("$id\t$name\t$english\t$math")
+        println("$id\t$name\t$english\t$math\t${average()}\t${grading()}")
     }
-    
+
+    fun average(): Int {
+        return (english + math) / 2
+    }
+
+    fun grading() = when (average() / 10) {
+        9, 10 -> "A"
+        8 -> "B"
+        7 -> "C"
+        6 -> "D"
+        else -> "F"
+    }
+
 }
 
 
 fun main() {
-    val stu1 = Student("001", "Jack", 70, 80)
+    val students = listOf<Student>(
+        Student("001", "Jack", 100, 100),
+        Student("002", "Hank", 66, 87),
+        Student("003", "Jane")
+    )
+    //meehod-3
+    for (stu in students){
+        stu.print()
+    }
+    //method-2
+    for (i in 0..2){
+        students.get(i).print()
+    }
+    //method-1
+    val stu1 = Student("001", "Jack", 100, 100)
     val stu2 = Student("002", "Hank", 66, 87)
     val stu3 = Student("003", "Jane")
     stu1.print()
