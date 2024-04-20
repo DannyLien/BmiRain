b4bc742c448f81dbc73c34cf0735b6eb1fa71fa3
DannyLien <danny.lien1109@gmail.com>
2024-04-20 21:22:26 +0800

    3-4, 物件導向設計, 改版 GuessGame.kt ; 2-13, 作業1;

--- app/src/main/java/com/hank/rainbmi/GuessGame.kt
+++ app/src/main/java/com/hank/rainbmi/GuessGame.kt
@@ -1,4 +1,5 @@
 package com.hank.rainbmi
+
 import kotlin.random.Random
 
 class GuessGame {
@@ -24,35 +25,47 @@ fun main() {
     while (playStatus == 1) {
         println("game.secret is : ${game.secret}")
         var message = 0
-        var result = ""
+//        var result = ""
         var bingo = false
+        var min = 1
+        var max = 10
         while (game.counter < 3) {
-            print("Please enter number 1-10 : ")
+//            print("Please enter number 1-10 : ")
+            print("Please enter a number ($min-$max) : ")
             val input = readLine()
             var num: Int = input?.toIntOrNull() ?: 0
-            println("The number you enter : $num")
-            message = game.guess(num)
-            result = when {
-                message > 0 -> "Bigger"
-                message < 0 -> "Smaller"
-                else -> "You got it!"
-            }
-            println(result)
-            if (message == 0) {
-                bingo = true
-                break
+//            println("The number you enter : $num")
+//            message = game.guess(num)
+//            result = when {
+//                message > 0 -> "Bigger"
+//                message < 0 -> "Smaller"
+//                else -> "You got it!"
+//            }
+//            println(result)
+            message = game.guess(num)   //secret - n
+            when {
+                message > 0 -> min = num    //Bigger
+                message < 0 -> max = num    //Smaller
+                else -> {       //bingo
+                    if (message == 0) {
+                        bingo = true
+                        break
+                    }
+                }
             }
         }
         if (!bingo) {
             println("Game Failed, Secret Number is : ${game.secret}")
+        } else {
+            println("You got it!")
         }
         println("Do your want play again ?? ")
         print("Enter 1 = Yes , other = No : ")
         val input = readLine()
         playStatus = input?.toIntOrNull() ?: 0
-        if (playStatus==1) {
+        if (playStatus == 1) {
             game.reset()
-        }else{
+        } else {
             println("End Game , Bye")
         }
 
