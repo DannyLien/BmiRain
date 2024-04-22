aedc52653ef1f6541f2ce8b7ac5a67de0898a5be
DannyLien <danny.lien1109@gmail.com>
2024-04-22 23:57:27 +0800

    5-7, LogCat, Toast, AlertDialog, Simple Guess;

--- app/src/main/java/com/hank/rainbmi/MainActivity2.kt
+++ app/src/main/java/com/hank/rainbmi/MainActivity2.kt
@@ -1,30 +1,35 @@
 package com.hank.rainbmi
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.hank.rainbmi.databinding.ActivityMainBinding
 import kotlin.math.log
+import kotlin.random.Random
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val secret = Random.nextInt(1, 11)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()  //要留
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }   //可註解
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }   //可註解
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Toast.makeText(this, "Secret number is $secret", Toast.LENGTH_LONG).show()
 
     }
 
@@ -32,6 +37,21 @@ class MainActivity2 : AppCompatActivity() {
         if (!binding.number.text.toString().equals("")) {
             val num = binding.number.text.toString().toInt()
             Log.d("Mainactivity2", num.toString())
+            var message = if (num > secret) {
+                "Smaller"
+            } else if (num < secret) {
+                "Bigger"
+            } else {
+                "You got it"
+            }
+            AlertDialog.Builder(this)
+                .setTitle("Info")
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .show()
+
+        } else {
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_LONG).show()
         }
     }
 
