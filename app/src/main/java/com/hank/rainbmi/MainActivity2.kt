0cd5d4f7ec43a71ca9d9d05b0191b55b9a5b0ff4
DannyLien <danny.lien1109@gmail.com>
2024-04-22 09:31:35 +0800

    5-6, 輸入, 按鈕, log.d觀察

--- app/src/main/java/com/hank/rainbmi/MainActivity2.kt
+++ app/src/main/java/com/hank/rainbmi/MainActivity2.kt
@@ -1,27 +1,38 @@
 package com.hank.rainbmi
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.hank.rainbmi.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity2 : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()  //要留
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }   //可註解
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }   //可註解
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
     }
+
+    fun guess(view: View) {
+        if (!binding.number.text.toString().equals("")) {
+            val num = binding.number.text.toString().toInt()
+            Log.d("Mainactivity2", num.toString())
+        }
+    }
+
 }
