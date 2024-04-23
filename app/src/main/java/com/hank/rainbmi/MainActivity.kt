b047e575c808faf215ae0be96a1bf7164064a368
DannyLien <danny.lien1109@gmail.com>
2024-04-23 22:52:34 +0800

    6-2, @Composable //宣告式元件設計方法

--- app/src/main/java/com/hank/rainbmi/MainActivity.kt
+++ app/src/main/java/com/hank/rainbmi/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.hank.rainbmi
 
+import android.graphics.drawable.Icon
+import android.hardware.lights.Light
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +31,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    SearchBar()
                 }
             }
         }
@@ -31,16 +41,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text(text = "Test2")
+        Text(text = "Test3 blablabla")
+    }
 }
 
-@Preview(showBackground = true)
+@Composable //宣告式畫面元件設計方法
+fun SearchBar(modifier: Modifier = Modifier) {
+    Column {    //layout
+        TextField(          //文字
+            value = "",
+            onValueChange = {},
+            placeholder = {
+                Text(text = "Search")
+            },
+            leadingIcon = {     //Icon
+                androidx.compose.material3.Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = ""
+                )
+            },
+            modifier = modifier.fillMaxWidth()  //設定寬度
+        )
+    }
+}
+
+@Preview(showBackground = true)  //監視畫面
 @Composable
-fun GreetingPreview() {
+fun MyPreview() {
     BmiRainTheme {
-        Greeting("Android")
+//       Greeting("Android")
+        SearchBar()
     }
 }
