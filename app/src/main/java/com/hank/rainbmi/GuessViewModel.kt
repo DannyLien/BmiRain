efd7c00f177d66914de0f3bae99bf52704b2fee8
DannyLien <danny.lien1109@gmail.com>
2024-04-27 10:47:51 +0800

    7-5, LiveData GuessViewModel counter 初始值, 動作, 按鈕 guess();

--- app/src/main/java/com/hank/rainbmi/GuessViewModel.kt
+++ app/src/main/java/com/hank/rainbmi/GuessViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GuessViewModel : ViewModel() {
+    //LiveData
     val counter = MutableLiveData<Int>()
 
     init {
--- app/src/main/java/com/hank/rainbmi/MainActivity2.kt
+++ app/src/main/java/com/hank/rainbmi/MainActivity2.kt
@@ -25,17 +25,12 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()  //要留
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }   //可註解
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //ViewModel
         viewModel = ViewModelProvider(this).get(GuessViewModel::class.java)
+        //LiveData
         viewModel.counter.observe(this, { counter ->
             binding.counter.text = counter.toString()
         })
@@ -53,30 +48,33 @@ class MainActivity2 : AppCompatActivity() {
 //    }
 
     fun guess(view: View) {
-        if (!binding.number.text.toString().equals("")) {
-            val num = binding.number.text.toString().toInt()
-            val message = when (game.guess(num)) {
-                GuessGame.Status.SMALLER -> getString(R.string.smaller)
-                GuessGame.Status.BIGGER -> getString(R.string.bigger)
-                else -> getString(R.string.you_got_it)
-            }
-            binding.counter.text = game.counter.toString()
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.info))
-                .setMessage(message)
-                //.setPositiveButton(getString(R.string.ok), okLisener)
-                .setPositiveButton(getString(R.string.ok), null)
-                .setNegativeButton("Replay", { dialog, which ->
-                    Log.d(TAG, "Replay")
-                    game.reset()
-                    binding.counter.text = game.counter.toString()
-                })
-                .show()
+        //viewModel
+        viewModel.guess(binding.number.text.toString().toInt())
 
-        } else {
-            Toast.makeText(this, getString(R.string.please_enter_a_number_1_10), Toast.LENGTH_LONG)
-                .show()
-        }
+//        if (!binding.number.text.toString().equals("")) {
+//            val num = binding.number.text.toString().toInt()
+//            val message = when (game.guess(num)) {
+//                GuessGame.Status.SMALLER -> getString(R.string.smaller)
+//                GuessGame.Status.BIGGER -> getString(R.string.bigger)
+//                else -> getString(R.string.you_got_it)
+//            }
+//            binding.counter.text = game.counter.toString()
+//            AlertDialog.Builder(this)
+//                .setTitle(getString(R.string.info))
+//                .setMessage(message)
+//                //.setPositiveButton(getString(R.string.ok), okLisener)
+//                .setPositiveButton(getString(R.string.ok), null)
+//                .setNegativeButton("Replay", { dialog, which ->
+//                    Log.d(TAG, "Replay")
+//                    game.reset()
+//                    binding.counter.text = game.counter.toString()
+//                })
+//                .show()
+//
+//        } else {
+//            Toast.makeText(this, getString(R.string.please_enter_a_number_1_10), Toast.LENGTH_LONG)
+//                .show()
+//        }
     }
 
 }
